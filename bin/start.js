5aff4c06d5a8392199855ac6474fd48d5a5b8627
shonie <shonie.starnikov@gmail.com>
2020-08-25 21:00:58 +0300

    Add server

--- bin/start.js
+++ bin/start.js
@@ -1,3 +1,19 @@
-import { helloWorld } from '../app/index.js';
+import http from "http";
 
-helloWorld();
+let hostname = process.env.HOST || "127.0.0.1";
+let port = process.env.PORT || 3000;
+
+let server = http.createServer((req, res) => {
+  console.log('Incoming request', req);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, world!");
+});
+
+server.on("error", (err) => {
+  console.log("Error happened", err);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
