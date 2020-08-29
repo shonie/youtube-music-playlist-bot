6de460cb594f446993094984f5f3e5dcec3bad30
shonie <shonie.starnikov@gmail.com>
2020-08-30 00:19:48 +0300

    Use bot-brother

--- bin/start.js
+++ bin/start.js
@@ -1,15 +1,3 @@
-import createDebug from 'debug';
-import * as server from '../lib/server.js';
-import * as app from '../lib/app.js';
+import { start } from '../lib/bot.js';
 
-let debug = createDebug('app:start');
-
-(async () => {
-  try {
-    await app.start();
-    await server.start();
-  } catch (err) {
-    debug('Error on start', err);
-    process.exit(1);
-  }
-})();
+start();
